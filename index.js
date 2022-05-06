c067c685c5760ffd85eb6ea34cbcef9b48b7c1c9
JamesNg <njames15@gmail.com>
2022-05-05 20:33:40 -0400

    commit tree cd6fdcb1dad0ac259e5ee9ae107c3afd6f9c4a03

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+  
+    // insert in body
+    // const formData = {
+    //   name : name, 
+    //   email : email,
+    // };
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email : email,
+        }),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(object => document.querySelector('body').textContent = object.id)
+      .catch(error => document.querySelector('body').textContent = error.message);
+    
+}
+
+submitData('Omar','James');
+
